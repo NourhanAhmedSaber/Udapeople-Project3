638b18f41d5e289fed1b2cea6dc50424369cf24e
NourhanAhmedSaber <nourhansaber57@gmail.com>
2022-12-24 15:20:24 +0200

    initial commit

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -1,9 +1,9 @@
 export NODE_ENV=production
 export VERSION=1
 export TYPEORM_CONNECTION=postgres
-export TYPEORM_MIGRATIONS_DIR=./migrations
-export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
-export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./src/migrations/*{.ts,.js}
 export TYPEORM_HOST=localhost
 export TYPEORM_PORT=5532
 export TYPEORM_USERNAME=postgres
